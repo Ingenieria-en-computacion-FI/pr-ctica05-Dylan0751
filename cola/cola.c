c3870de352f1486f8b27aed57a4e799213afe07c
Dylan0751 <121585029+Dylan0751@users.noreply.github.com>
2026-03-15 18:39:30 +0000

    commitcola

--- cola/cola.c
+++ cola/cola.c
@@ -1,48 +1,39 @@
 #include "cola.h"
 
-Cola* cola_crear()
-{
+Cola* cola_crear(){
+    
     return lista_crear();
 }
 
-int cola_vacia(Cola* cola)
-{
-    /*
-    TODO
-    usar vaciar_lista
-    */
+int cola_vacia(Cola* cola){
+    
+    return lista_vacia(cola);
+   
 }
 
-void cola_enqueue(Cola* cola, int dato)
-{
-    /*
-    TODO
-    usar lista_insertar_tail
-    */
+void cola_enqueue(Cola* cola, int dato){
+   
+    lista_insertar_tail(cola, dato);
+    
 }
 
-int cola_dequeue(Cola* cola)
-{
-    /*
-    TODO
-    usar lista_eliminar_head
-    */
-    return -1;
+int cola_dequeue(Cola* cola){
+    
+    return lista_eliminar_head(cola);
+    
 }
 
-int cola_frente(Cola* cola)
-{
-    /*
-    TODO
-    regresar dato del head
-    */
-    return -1;
+int cola_frente(Cola* cola){
+   
+    if(cola_vacia(cola)){
+        return -1;
+    }
+    return cola->head->dato;
+    
 }
 
-void cola_destruir(Cola* cola)
-{
-    /*
-    TODO
-    usar lista_destruir
-    */
+void cola_destruir(Cola* cola){
+   
+    lista_destruir(cola);
+    
 }
